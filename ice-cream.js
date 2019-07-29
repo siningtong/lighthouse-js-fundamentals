4bfdf1ce7cc808e551ff06b9185bcb5c4ac3e630
siningtong <siningtong@gmail.com>
2019-07-29 18:25:28 +0000

    ice cream flavours

---
+++ ice-cream.js
@@ -0,0 +1,7 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+iceCreamFlavours.push('root bear');
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[5]);
+console.log(iceCreamFlavours.length);
